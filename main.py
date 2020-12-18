ca4ef0c1d4e11a017853ffaf8d6626c41c3a80f0
Imran Akbar <infotainer04@gmail.com>
2020-12-18 11:57:42 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Twitter Bot Python
+# Importing Modules
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
